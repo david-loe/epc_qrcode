a3fe214b2875d2c058c8915dbee2e93bff9a81a4
david-loe <56305409+david-loe@users.noreply.github.com>
2024-08-04 18:21:53 +0000

    fix gen

--- epc_qrcode/gen.py
+++ epc_qrcode/gen.py
@@ -1,7 +1,22 @@
 from segno import helpers
 import frappe
 
+
 @frappe.whitelist()
-def gen_epcqrcode(**kwargs):
-    qrcode = helpers.make_epc_qr(kwargs)
-    return qrcode.png_data_uri()
+def gen_epcqrcode(
+    name,
+    iban,
+    amount,
+    text=None,
+    reference=None,
+    bic=None,
+    purpose=None,
+    encoding=None,
+    scale=3,
+    dark="#000",
+    light="#fff",
+):
+    qrcode = helpers.make_epc_qr(
+        name=name, iban=iban, amount=amount, text=text, reference=reference, bic=bic, purpose=purpose, encoding=encoding
+    )
+    return qrcode.png_data_uri(scale=scale, dark=dark, light=light)
